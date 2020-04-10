d30fd01d5818d02999646cc3fb91408522ecffd0
John_L <john6.lq@gmail.com>
2020-04-10 11:29:29 +0800

    TopToast added

--- README.md
+++ README.md
@@ -5,5 +5,8 @@
 [RecyclerView通讯录粘性头部/吸顶效果(ItemDecoration实现](https://www.jianshu.com/p/b5137b9b051d)
 ![最终效果](https://upload-images.jianshu.io/upload_images/14730476-103c3881a09692bb.gif?imageMogr2/auto-orient/strip)
 
-### `InfiniteScrollActivity.kt`  2020-04-03
+### `InfiniteScrollActivity.kt`  2020-04-08
 无限循环Banner(RecyclerView、Int.MAX_VALUE实现)，无限滚动，丝滑流畅。
+
+### `TopToast.kt` 2020-04-10
+[自定义Toast，宽度全屏，高度自定义，显示在titleBar下方](https://www.jianshu.com/p/7d9ef7db1502)
---
+++ TopToast.kt
@@ -0,0 +1,49 @@
+import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+
+class TopToast(val context: Context) :Toast(context){
+
+    private var topText: String = ""
+    private val museView: TextView = TextView(context).apply {
+        setBackgroundColor(Color.parseColor("#BB725A7C"))
+        gravity = Gravity.CENTER
+        setTextColor(ContextCompat.getColor(context, android.R.color.white))
+        setTextIsSelectable(false)
+        layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MuseUtil.newDp2px(40f))
+    }
+
+    init {
+        view = FrameLayout(context).apply {
+            addView(museView)
+	    // 不设置会默认留出状态栏高度的距离
+            systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+	// 设置顶部titleBar高度的偏移
+        setGravity(Gravity.TOP+Gravity.FILL_HORIZONTAL, 0, MuseUtil.newDp2px(64f))
+    }
+
+    fun setTopText(text:String):TopToast{
+        topText = text
+        return this
+    }
+    fun setTopText(@StringRes stringRes:Int):TopToast{
+        topText = context.getString(stringRes)
+        return this
+    }
+       
+    override fun getDuration(): Int = LENGTH_SHORT
+
+    override fun show() {
+        museView.text = topText
+        super.show()
+    }
+
+}
